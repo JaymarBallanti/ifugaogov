61dcbb7d917643ddd7935328baee12ef4d328226
Jaymar Ballanti <Jaymar Ballanti>
2016-10-15 07:31:24 +0800

    first commit

---
+++ administrator/cache/regularlabs/21c537391b2d2bd42097b44cacb36203-cache-regularlabs-27b57e5cc4f52622c0fd610a4b920d97.php
@@ -0,0 +1,17 @@
+<?php die("Access Denied"); ?>#x#s:1304:"<?xml version="1.0" encoding="utf-8"?>
+<extensions>
+	<extension>
+		<name>Advanced Module Manager</name>
+		<id>advancedmodulemanager</id>
+		<alias>advancedmodulemanager</alias>
+		<extname>advancedmodules</extname>
+		<version>6.2.5</version>
+		<has_pro>1</has_pro>
+		<pro>0</pro>
+		<downloadurl>http://download.regularlabs.com/?ext=advancedmodulemanager</downloadurl>
+		<downloadurl_pro></downloadurl_pro>
+		<changelog><![CDATA[
+<div style="font-family:monospace;font-size:1.2em;">06-Oct-2016 : v6.2.5<br /> + Adds translations: nl-BE<br /> ^ Updated code to include latest fixes for core module manager for Joomla 3.6.3<br /> ^ Updates translations: pt-PT<br /> # Fixes issue with Hide if Empty option seeing modules with images as empty<br /> # Fixes issue with assignments not working on raw/ajax views<br /> # Fixes issue with potential php notices when using the menu item assignments<br /><br />05-Sep-2016 : v6.2.4<br /> # Fixes issue with administrator returning blank on some setups<br /><br />01-Sep-2016 : v6.2.3<br /> # Fixes issue with assignment to tags not working on tag pages<br /> # Updates browser detection scripts<br /> # Updates list of browsers in Browser Assignment</div><div style="text-align: right;"><i>...click for more...</i></div>
+		]]></changelog>
+	</extension>
+</extensions>";
